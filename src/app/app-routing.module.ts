9e5389f172d7e77140787e8ea63059c7bea719eb
Angel <118619693+GWhiteWolf@users.noreply.github.com>
2024-08-27 23:19:10 -0400

    Maquetación de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'informe',
     loadChildren: () => import('./pages/informe/informe.module').then( m => m.InformePageModule)
+  },
+  {
+    path: 'logros',
+    loadChildren: () => import('./pages/logros/logros.module').then( m => m.LogrosPageModule)
   },
 ];
 
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+//Modelo para obtener los inputs del login
+  login:any={
+    usuario:"",
+    password:""
+  }
+field:string="";
 
-  constructor() { }
+  constructor(public router:Router, public toastController:ToastController) { }
 
   ngOnInit() {
   }
 
+  ingresar(){
+    if(this.validateModel(this.login)){
+      this.presentToast("top", "Bienvenid@!")
+      this.router.navigate(['/home']);
+    }else{
+      this.presentToast("bottom", "Error: Falta:" + this.field, 4000);
+    }
+  }
+
+  validateModel(model:any){
+    for(var [key, value] of Object.entries(model)){
+      if(value == ""){
+        this.field = key;
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  async presentToast(position: 'top' | 'middle' | 'bottom', msg:string, duration?:number) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: duration?duration:2500,
+      position: position,
+    });
+
+    await toast.present();
+  }
 }
