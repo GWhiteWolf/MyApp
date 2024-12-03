17f32d284459200b87bc1fc477a7aa6cdc4a4280
ReDiego0 <134623797+ReDiego0@users.noreply.github.com>
2024-12-03 04:17:05 -0300

    Apk creada y firmada

---
+++ android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marcapasos.app',
+  appName: 'Marcapasos',
+  webDir: 'http://localhost:8100/login'
+};
+
+export default config;
